588bed9096f9c9e18e838660f46ce05bf3b010b5
ankittojha <ankitojha1409@gmail.com>
2022-07-16 23:53:31 +0530

    custom js added

---
+++ chat-bot/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".chat-icon").click(function (event) {
+    $("chat-box").toggleClass("active");
+  });
+  $(".conv-form-wrapper").convform({ selectInputStyle: "disable" });
+});
